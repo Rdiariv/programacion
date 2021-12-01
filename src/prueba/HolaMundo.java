11a959a48e1881e36aa54422f4ba50bb736c8052
Rubén Díaz <rdr1982@gmail.com>
2021-12-01 11:32:15 +0000

    prueba

---
+++ src/prueba/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prueba;
+
+/**
+ * @author ruben
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
